31ba71996d68338c2fcc5944298928b12addabe0
amityohan <amit.yohan@kalvium.community>
2024-12-12 11:24:49 +0530

    finished assigment

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 8080;
+require("dotenv").config()
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
